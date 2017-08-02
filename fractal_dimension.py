ff24ba60124ce7e0881cfea2c165540031136706
Peter <username@users.noreply.github.com>
2017-08-02 21:11:24 +0100

    Change in box distributution
    Fixes #1, fixes #2.
    
    Change in the way boxes are distributed, now starts with 4 pixel boxes and increases, scanning all possible pixel values. Also normalises answer based on how much of the image was covered by boxes.

--- fractal_dimension.py
+++ fractal_dimension.py
@@ -16,7 +16,7 @@ def plot_smooth(x, y, style):
     return popt
 
 
-def main2d(image_name, num_boxes, average_threshold, minimum_threshold):
+def main2d(image_name, average_threshold, minimum_threshold, debug=True):
     """main function for determining the fractal dimension of 2d datasets"""
 
     tmp_image = Image.open(image_name)
@@ -26,15 +26,17 @@ def main2d(image_name, num_boxes, average_threshold, minimum_threshold):
         tmp_image = tmp_image/256
     image_data = tmp_image
 
-    dimensions = []
+    dimensions = np.zeros((int(image_size[0]/2), 2))
 
-    for box_size in range(2, num_boxes):
-        dimensions.append(count_boxes_2d(image_data, box_size, image_size, average_threshold, minimum_threshold))
+    for box_size in range(4, int(image_size[0]/2)):
+        dimensions[box_size, 1] = count_boxes_2d(image_data, box_size, image_size, average_threshold, minimum_threshold, debug)
+        # calculate the magnification factor
+        dimensions[box_size, 0] = image_size[0]/box_size
 
-    plot_dimension(num_boxes, dimensions)
+    plot_dimension(dimensions)
 
 
-def main3d(image_directory, file_prefix, file_suffix, num_images, num_boxes, average_threshold, minimum_threshold):
+def main3d(image_directory, file_prefix, file_suffix, num_images, average_threshold, minimum_threshold):
     """main function for determining the fractal dimension of 3D datasets"""
 
     image_size = setup_load_images(num_images, image_directory, file_prefix, file_suffix)
@@ -47,18 +49,20 @@ def main3d(image_directory, file_prefix, file_suffix, num_images, num_boxes, ave
             tmp_image = tmp_image / 256
         image_data[i] = tmp_image
 
-    dimensions = []
+    dimensions = np.zeros((int(image_size[0] / 2), 2))
 
-    for box_size in range(2, num_boxes):
-        dimensions.append(count_boxes_3d(image_data, box_size, image_size, average_threshold, minimum_threshold))
+    for box_size in range(4, int(image_size[0]/2)):
+        dimensions[box_size, 1] = count_boxes_3d(image_data, box_size, image_size, average_threshold, minimum_threshold)
+        # calculate the magnification factor
+        dimensions[box_size, 0] = image_size[0]/box_size
 
-    plot_dimension(num_boxes, dimensions)
+    plot_dimension(dimensions)
 
 
-def plot_dimension(num_boxes, dimensions):
+def plot_dimension(dimensions):
     """ Plot filled boxes against box magnification. The gradient is the fractal dimension"""
-    plt.loglog(np.arange(2, num_boxes), dimensions, 'x')
-    gradient = plot_smooth(np.arange(2, num_boxes), dimensions, '-k')
+    plt.loglog(dimensions[4:, 0], dimensions[4:, 1], 'x')
+    gradient = plot_smooth(dimensions[4:, 0], dimensions[4:, 1], '-k')
     plt.xlabel("magnification")
     plt.ylabel("filled boxes")
     plt.text(5, 10, "Fractal dimension = " + '{:1.2f}'.format(gradient[1]))
@@ -66,57 +70,70 @@ def plot_dimension(num_boxes, dimensions):
     np.savetxt("fractal_dimension.txt", dimensions)
 
 
-def count_boxes_2d(image_data, box_size, image_size, average_threshold, minimum_threshold, debug=True):
+def count_boxes_2d(image_data, box_size, image_size, average_threshold, minimum_threshold, debug):
     """ Box counting for 2d images"""
-    box_x_size = int(image_size[0] / box_size)
-    box_y_size = int(image_size[1] / box_size)
-    box_area = box_x_size * box_y_size * 0.1
+    # Box_size is the box side length in pixels
+    box_area = box_size * box_size * 0.1
     filled_boxes = 0
+    max_x_boxes = int(image_size[0] / box_size)
+    max_y_boxes = int(image_size[1] / box_size)
+    area_scanned = (max_x_boxes * box_size * max_y_boxes * box_size)/(image_size[0]*image_size[1])
+
     if debug:
         plt.imshow(image_data)
         myax = plt.gca()
+        if box_size < 8:
+            debug_width = 1
+        else:
+            debug_width = 2
 
-    for x_box_num in range(0, box_size):
-        for y_box_num in range(0, box_size):
-            x_min = x_box_num * box_x_size
-            x_max = ((x_box_num + 1) * box_x_size)-1
-            y_min = y_box_num * box_y_size
-            y_max = (y_box_num + 1) * box_y_size
+    for x_box_num in range(0, max_x_boxes):
+        for y_box_num in range(0, max_y_boxes):
+            x_min = x_box_num * box_size
+            x_max = (x_box_num + 1) * box_size
+            y_min = y_box_num * box_size
+            y_max = (y_box_num + 1) * box_size
             if np.average(image_data[x_min:x_max, y_min:y_max]) > average_threshold:
                 if (np.count_nonzero(image_data[x_min:x_max, y_min:y_max] > minimum_threshold)) > box_area:
                     filled_boxes += 1
                     if debug:
-                        myax.add_patch(patches.Rectangle((y_min+2, x_min+2), box_x_size-4, box_y_size-4, fill=False, edgecolor="green", linewidth=2))
+                        myax.add_patch(patches.Rectangle((y_min+debug_width, x_min+debug_width), box_size-debug_width*2,
+                                                         box_size-debug_width*2, fill=False, edgecolor="green",
+                                                         linewidth=debug_width))
                 else:
                     if debug:
-                        myax.add_patch(patches.Rectangle((y_min+2, x_min+2), box_x_size-4, box_y_size-4, fill=False, edgecolor="red", linewidth=2))
+                        myax.add_patch(patches.Rectangle((y_min+debug_width, x_min+debug_width), box_size-debug_width*2,
+                                                         box_size-debug_width*2, fill=False, edgecolor="red",
+                                                         linewidth=debug_width))
     plt.show()
 
-    return filled_boxes
+    return filled_boxes/area_scanned
 
 
 def count_boxes_3d(image_data, box_size, image_size, average_threshold, minimum_threshold):
     """box counting for 3d images"""
-    box_x_size = int(image_size[0]/box_size)
-    box_y_size = int(image_size[1]/box_size)
-    box_z_size = int(image_size[2]/box_size)
-    box_area = box_x_size * box_y_size * box_x_size*0.05
-
+    box_area = (box_size ** 3) * 0.05   # The fraction of the box that must be filled with pixels > minimum threshold
     filled_boxes = 0
+    max_x_boxes = int(image_size[0] / box_size)
+    max_y_boxes = int(image_size[0] / box_size)
+    max_z_boxes = int(image_size[0] / box_size)
+    area_scanned = (max_x_boxes * box_size * max_y_boxes * box_size * max_z_boxes * box_size) \
+                   / (image_size[0] * image_size[1] * image_size[2])
 
-    for x_box_num in range(0, box_size):
-        for y_box_num in range(0, box_size):
-            for z_box_num in range(0, box_size):
-                x_min = x_box_num * box_x_size
-                x_max = ((x_box_num + 1) * box_x_size)-1
-                y_min = y_box_num * box_y_size
-                y_max = (y_box_num + 1) * box_y_size
-                z_min = z_box_num * box_z_size
-                z_max = (z_box_num + 1) * box_z_size
+    for x_box_num in range(0, max_x_boxes):
+        for y_box_num in range(0, max_y_boxes):
+            for z_box_num in range(0, max_z_boxes):
+                x_min = x_box_num * box_size
+                x_max = (x_box_num + 1) * box_size
+                y_min = y_box_num * box_size
+                y_max = (y_box_num + 1) * box_size
+                z_min = z_box_num * box_size
+                z_max = (z_box_num + 1) * box_size
                 if np.average(image_data[z_min:z_max, x_min:x_max, y_min:y_max]) > average_threshold:
                     if (np.count_nonzero(image_data[z_min:z_max, x_min:x_max, y_min:y_max] > minimum_threshold)) > box_area:
                         filled_boxes += 1
-    return filled_boxes
+
+    return filled_boxes/area_scanned
 
 
 def setup_load_images(num_images, image_directory, file_prefix, file_suffix):
